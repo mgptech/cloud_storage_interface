c38058cbf94e1034f772c54c7fad284227cc2caf
maxpleaner <maxpleaner@gmail.com>
2019-09-17 11:26:16 -0700

    fix GCS presigned post and bump version

--- lib/cloud_storage_interface/gcp_gcs_interface.rb
+++ lib/cloud_storage_interface/gcp_gcs_interface.rb
@@ -56,8 +56,18 @@ class CloudStorageInterface::GcpGcsInterface
 
     # https://cloud.google.com/storage/docs/xml-api/post-object#usage_and_examples
     # https://www.rubydoc.info/gems/google-cloud-storage/1.0.1/Google/Cloud/Storage/Bucket:post_object
-    def presigned_post(bucket_name:, key:, **opts)
-      post_obj = get_bucket!(bucket_name).post_object(key, policy: opts)
+    def presigned_post(bucket_name:, key:, acl:, success_action_status:, expiration: nil)
+      expiration ||= (Time.now + 1.hour).iso8601
+
+      policy = {
+        expiration: expiration,
+        conditions: [
+          { acl: acl },
+          { success_action_status: success_action_status }
+        ]
+      }
+
+      post_obj = get_bucket!(bucket_name).post_object(key, policy: policy)
 
       url_obj = { host: URI.parse(post_obj.url).host }
       
