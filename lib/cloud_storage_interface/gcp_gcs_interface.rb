8ec2738769eeae435a79d76ac609d5d91849cbc7
Kapil Dewade <kapil@edcast.com>
2019-11-14 11:32:32 -0800

    EP-30873 Fix acl

--- lib/cloud_storage_interface/gcp_gcs_interface.rb
+++ lib/cloud_storage_interface/gcp_gcs_interface.rb
@@ -19,6 +19,10 @@ class CloudStorageInterface::GcpGcsInterface
     # we also don't return the checksum here.
     # NOTE: This will overwrite the file if the key already exists
     def upload_file(bucket_name:, key:, file:, **opts)
+      if opts[:acl] && opts[:acl]&.to_s == 'public-read'
+        opts[:acl] = :public_read
+      end
+
       result = get_bucket!(bucket_name).create_file file.path, key, **opts
       return {
         checksum: result.crc32c
